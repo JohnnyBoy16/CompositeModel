cb3025e8a578f7e21db6b0d5be6939f46b6a3f86
John Nagel <jnagel@iastate.edu>
2017-11-28 00:32:56 -0600

    Working on Void Location

--- final_project.py
+++ final_project.py
@@ -6,8 +6,17 @@ from matplotlib.collections import PatchCollection
 # the number of layers in the composite
 n_layers = 10
 
-# thickness of the layers in mm
-t_layer = 1
+# thickness of the sample in mm
+# thickness = 6.35  # 6.35 mm = 1/4 inch
+
+# thickness of the fiberglass layers in mm
+t_fiberglass = 0.25
+
+# thickness of the epoxy layers in mm
+t_epoxy = 0.05
+
+# thickness of the sample in mm
+thickness = n_layers*(t_fiberglass+t_epoxy) - 2*t_epoxy
 
 # diameter of the beam in mm
 d_beam = 1.5
@@ -30,7 +39,10 @@ x_centers = np.random.rand(n_voids) * width
 # y location of the void center
 # assume that voids can only occur at layer boundaries, so this is the layer at
 # which the void will occur
-y_centers = np.random.randint(1, n_layers, n_voids)
+
+# random.randint() high is exclusive
+y_centers_int = np.random.randint(low=1, high=n_layers, size=n_voids).astype(float)
+y_centers = y_centers_int * ((t_fiberglass + t_epoxy) - (t_epoxy / 2))
 
 x_corners = x_centers - void_size[0] / 2
 y_corners = y_centers - void_size[1] / 2
@@ -51,7 +63,8 @@ pc = PatchCollection(void_collection, alpha=0.5)
 fig = plt.figure('Location of void centers')
 axis = fig.add_subplot(111)
 for i in range(n_layers+1):  # put a line at each layer boundary
-    plt.axhline(i, color='k', linestyle='--', linewidth=0.5)
+    layer_height = i * ((t_fiberglass+t_epoxy) - (t_epoxy/2))
+    plt.axhline(layer_height, color='k', linestyle='--', linewidth=0.5)
 
 # add the voids to the plot
 axis.add_collection(pc)
@@ -67,6 +80,6 @@ plt.xlabel('X Location (mm)')
 plt.ylabel('Layer Boundary')
 plt.title('Diagram of Simulation')
 plt.xlim(0, width)
-plt.yticks(np.arange(0, n_layers+1))
-plt.ylim(n_layers, 0)  # flip y-axis so layer 0 is on top
+# plt.yticks(np.arange(0, n_layers+1))
+plt.ylim(thickness, 0)  # flip y-axis so layer 0 is on top
 plt.legend()
